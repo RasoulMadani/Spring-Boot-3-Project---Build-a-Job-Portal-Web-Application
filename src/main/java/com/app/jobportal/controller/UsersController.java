3492e8ad7546a2028e231712c0ee82b467c8777b
Rasoulmadani <ali.rahbary@chmail.ir>
2024-08-25 10:41:55 +0330

    نوشتن کنترلر برای دریافت اطلاعات ثبت نام کاربر

--- src/main/java/com/app/jobportal/controller/UsersController.java
+++ src/main/java/com/app/jobportal/controller/UsersController.java
@@ -3,10 +3,12 @@ package com.app.jobportal.controller;
 import com.app.jobportal.entity.Users;
 import com.app.jobportal.entity.UsersType;
 import com.app.jobportal.services.UsersTypeService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -16,10 +18,16 @@ public class UsersController {
     private final UsersTypeService usersTypeService;
 
     @GetMapping("/register")
-     public String register(Model model){
-         List<UsersType> all = usersTypeService.getAll();
-         model.addAttribute("getAllTypes",all);
-         model.addAttribute("user",new Users());
+    public String register(Model model) {
+        List<UsersType> all = usersTypeService.getAll();
+        model.addAttribute("getAllTypes", all);
+        model.addAttribute("user", new Users());
         return "register";
-     }
+    }
+    @PostMapping("/register/new")
+    public String usersRegistration(@Valid Users users){
+        System.out.println("user" + users) ;
+        return "dashboard";
+    }
+
 }
