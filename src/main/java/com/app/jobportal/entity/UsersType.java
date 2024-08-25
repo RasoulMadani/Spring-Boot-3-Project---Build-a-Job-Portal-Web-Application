5b9707bbbdede8b11bcd4610a1612fe73e56d1ad
Rasoulmadani <ali.rahbary@chmail.ir>
2024-08-25 09:36:14 +0330

    ساخت انتیتی یوزر تایپ برای نقش ها

--- src/main/java/com/app/jobportal/entity/UsersType.java
+++ src/main/java/com/app/jobportal/entity/UsersType.java
@@ -1,8 +1,25 @@
 package com.app.jobportal.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.util.List;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString(exclude = {"users"})
+@Entity
+@Table(name = "users_type")
 public class UsersType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int userTypeId;
+
+    private String userTypeName;
+
+    @OneToMany(targetEntity = UsersType.class,mappedBy = "userTypeId",cascade = CascadeType.ALL)
+    private List<Users> users;
 }
