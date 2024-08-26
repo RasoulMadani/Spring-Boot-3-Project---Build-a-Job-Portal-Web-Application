66d07dbb0137bf90a505c1c0ee9f1f445a5bb3ce
Rasoulmadani <ali.rahbary@chmail.ir>
2024-08-26 11:44:40 +0330

    تکمیل انتیتی مهارت ها

--- src/main/java/com/app/jobportal/entity/Skills.java
+++ src/main/java/com/app/jobportal/entity/Skills.java
@@ -1,9 +1,25 @@
 package com.app.jobportal.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+
 @Table(name = "skills")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Skills {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String experienceLevel;
+    private String yearsOfExperience;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "job_seeker_profile")
+    private JobSeekerProfile jobSeekerProfile;
 }
