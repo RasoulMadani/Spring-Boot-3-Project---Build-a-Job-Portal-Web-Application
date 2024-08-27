a0eace523b268bc426bc784d08edbbb2967ead65
Rasoulmadani <ali.rahbary@chmail.ir>
2024-08-27 16:02:45 +0330

    برگرداندن نوع پروفایل براساس لاگینی که انجام شده است

--- src/main/java/com/app/jobportal/services/UsersService.java
+++ src/main/java/com/app/jobportal/services/UsersService.java
@@ -7,6 +7,11 @@ import com.app.jobportal.repository.JobSeekerProfileRepository;
 import com.app.jobportal.repository.RecruiterProfileRepository;
 import com.app.jobportal.repository.UsersRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -37,5 +42,22 @@ public class UsersService {
     }
     public Optional<Users> getUserByEmail(String email){
         return usersRepository.findByEmail(email);
+    }
+
+    public Object getCurrentUserProfile() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(!(authentication instanceof AnonymousAuthenticationToken)){
+            String username = authentication.getName();
+            Users users = usersRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("could not found user"));
+
+            int userId = users.getUserId();
+            if(authentication.getAuthorities().contains(new SimpleGrantedAuthority("Recruiter"))){
+               return recruiterProfileRepository.findById(userId).orElse(new RecruiterProfile());
+
+            }else {
+                return jobSeekerProfileRepository.findById(userId).orElse(new JobSeekerProfile());
+            }
+        }
+        return null;
     }
 }
